ef083e93cb94d08d0bcec2f9b37bb11a50fcea43
riteshbari25cs-cmyk <riteshbari.25cs@aryacollege.in>
2026-06-30 15:59:06 +0530

    Add files via upload

---
+++ 6 day/pr1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    int count = 0;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    for(int i = 0; i < str.length(); i++) {
+        char ch = str[i];
+
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ||
+           ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U') {
+            count++;
+        }
+    }
+
+    cout << "Number of vowels = " << count;
+
+    return 0;
+}
---
+++ 6 day/pr4.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    int words = 1;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    for(int i = 0; i < str.length(); i++) {
+        if(str[i] == ' ') {
+            words++;
+        }
+    }
+
+    cout << "Number of Words = " << words;
+
+    return 0;
+}
